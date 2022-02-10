a513ef3b2a2c6e50214d1afa32b7e45112ecf724
Cosmin Tanislav <demonsingur@gmail.com>
2022-02-10 16:37:05 +0200

    techpack: display: dsi_panel: expose attribute to notify of fod changes
    Change-Id: I6bdb767567c09e1a4cbd3b2d626e1173baf9089f

--- techpack/display/msm/dsi/dsi_panel.h
+++ techpack/display/msm/dsi/dsi_panel.h
@@ -370,6 +370,7 @@ struct dsi_panel {
 	unsigned int fod_dim_lut_len;
 	u8 fod_dim_alpha;
 	bool fod_hbm_enabled;
+	bool fod_ui;
 };
 
 static inline bool dsi_panel_ulps_feature_enabled(struct dsi_panel *panel)
@@ -516,5 +517,7 @@ int zte_dsi_panel_set_fps(struct dsi_panel *panel,int fps);
 #endif
 
 int dsi_panel_set_fod_hbm(struct dsi_panel *panel, bool status);
+bool dsi_panel_get_fod_ui(struct dsi_panel *panel);
+void dsi_panel_set_fod_ui(struct dsi_panel *panel, bool status);
 
 #endif /* _DSI_PANEL_H_ */
