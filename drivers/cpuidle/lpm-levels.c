6892b5b6c22a3655b1305bfaafd68f937721bf5d
anrui2032 <anrui2032@gmail.com>
2026-06-16 09:54:44 +0800

    cpuidle: Import ZTE changes
    Change-Id: I6e172e85d13c242d485061a9cbef1039cf9a9d00

--- drivers/cpuidle/lpm-levels.c
+++ drivers/cpuidle/lpm-levels.c
@@ -1729,6 +1729,11 @@ static void lpm_suspend_wake(void)
 	lpm_stats_suspend_exit();
 }
 
+/* zte_pm */
+/* cat /d/zte_gpio/dump_sleep_gpios */
+extern void zte_pm_vendor_before_powercollapse(void) __attribute__((weak));
+/* zte_pm */
+
 static int lpm_suspend_enter(suspend_state_t state)
 {
 	int cpu = raw_smp_processor_id();
@@ -1749,6 +1754,9 @@ static int lpm_suspend_enter(suspend_state_t state)
 	}
 	cpu_prepare(lpm_cpu, idx, false);
 	cluster_prepare(cluster, cpumask, idx, false, 0);
+
+	/* zte_pm add: suspend->PC, dump sleep gpios */
+	zte_pm_vendor_before_powercollapse();
 
 	disable_rimps_timer(lpm_cpu);
 	ret = psci_enter_sleep(lpm_cpu, idx, false);
